08e3675ce573a1c8de6d52ba35a72cd4499702f9
piz <pizegg@gmail.com>
2018-08-27 15:37:47 +0800

    [What] Add toString for PluginVer

--- sitersdk/src/main/java/uk/co/siterwell/sitersdk/PluginItem.kt
+++ sitersdk/src/main/java/uk/co/siterwell/sitersdk/PluginItem.kt
@@ -1,6 +1,10 @@
 package uk.co.siterwell.sitersdk
 
-class PluginVer(var majorVer: Int, var middleVer: Int, var minVer: Int)
+class PluginVer(var majorVer: Int, var middleVer: Int, var minVer: Int) {
+    override fun toString() : String {
+        return  "$majorVer.$middleVer+.$minVer"
+    }
+}
 
 class PluginItem(val pluginAppName: String,
                  val pluginDisplayName: String,
