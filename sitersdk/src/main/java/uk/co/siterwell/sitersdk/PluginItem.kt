2a81bc3803e707656f1f1295e9d372f1aafd41f0
piz <pizegg@gmail.com>
2018-08-29 14:12:14 +0800

    [What] overload PluginVer constructor

--- sitersdk/src/main/java/uk/co/siterwell/sitersdk/PluginItem.kt
+++ sitersdk/src/main/java/uk/co/siterwell/sitersdk/PluginItem.kt
@@ -1,6 +1,26 @@
 package uk.co.siterwell.sitersdk
 
-class PluginVer(var majorVer: Int, var middleVer: Int, var minVer: Int) {
+class PluginVer {
+    var majorVer = 0
+    var middleVer = 0
+    var minVer = 0
+    constructor(majorVer: Int, middleVer: Int, minVer: Int) {
+        this.majorVer = majorVer
+        this.middleVer = middleVer
+        this.minVer = minVer
+    }
+
+    constructor(version: String) {
+        val splitStr = version.split(".")
+        splitStr.forEachIndexed{index, _ver ->
+            when (index) {
+                0 -> majorVer = _ver.toInt()
+                1 -> middleVer = _ver.toInt()
+                2 -> minVer = _ver.toInt()
+            }
+        }
+    }
+
     override fun toString() : String {
         return  "$majorVer.$middleVer.$minVer"
     }
